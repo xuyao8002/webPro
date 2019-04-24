fd52a0d8381dbcb2b5eec945646c9bf79e9ba654
yao.xu <yao.xu@saiwangyun.com>
2019-04-24 18:16:11 +0800

    手动提交事务

--- src/main/java/com/springPro/service/impl/BaseServiceImpl.java
+++ src/main/java/com/springPro/service/impl/BaseServiceImpl.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Service;
 
 import com.springPro.dao.BaseDao;
 import com.springPro.entity.Entity;
 import com.springPro.service.BaseService;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 @Service
 public class BaseServiceImpl implements BaseService {
@@ -48,7 +52,21 @@ public class BaseServiceImpl implements BaseService {
 		}
 	}
 
+	@Autowired
+	private DataSourceTransactionManager transactionManager;
 
+	public void txManual(){
+		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
+		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);// 事物隔离级别，开启新事务
+		TransactionStatus status = transactionManager.getTransaction(def); // 获得事务状态
+		try{
+			//TODO
+			transactionManager.commit(status);
+		}catch(Exception e){
+			transactionManager.rollback(status);
+			throw e;
+		}
+	}
 	
 	
 }
